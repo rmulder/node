af51da08c7e98b8ed0298e316ae84835b149427e
sivachallapalli <siva.apps@icloud.com>
2019-05-16 15:09:13 -0500

    http module

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 const EventEmitter = require('events');
-
 const Logger = require('./logger');
 const logger = new Logger();
 
-//Register a listener
-logger.on('messageLogged', (arg) => { //e, eventArg
-    console.log('Listener called', arg);
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
 });
 
+// server.on('connection', (socket) => {
+//     console.log('New connection...');
+// });
 
-logger.log('message');
+server.listen(3000);
+
+console.log('Listening on port 3000...');
