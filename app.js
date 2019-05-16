30e374a25d0a0bf2ff46c16eee72e2536f193e13
sivachallapalli <siva.apps@icloud.com>
2019-05-16 09:58:09 -0500

    path module

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const path = require('path');
+
+var pathObj = path.parse(__filename);
 const log = require('./logger');
 
-log('message');
+log(pathObj);
--- logger.js
+++ logger.js
@@ -1,7 +1,3 @@
-console.log(__filename);
-console.log(__dirname);
-
-
 var url = 'http://mylogger.io/log';
 
 function log(message) {
