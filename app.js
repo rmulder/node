fc191a0d5cee56c5dc5ff145cdfe2299fc5a800d
sivachallapalli <siva.apps@icloud.com>
2019-05-16 14:44:33 -0500

    extending eventEmitter

--- app.js
+++ app.js
@@ -3,26 +3,14 @@ const os = require('os');
 const fs = require('fs');
 const EventEmitter = require('events');
 
-const emitter = new EventEmitter();
-
+const Logger = require('./logger');
+const logger = new Logger();
 
 //Register a listener
-emitter.on('messageLogged', (arg) => { //e, eventArg
+logger.on('messageLogged', (arg) => { //e, eventArg
     console.log('Listener called', arg);
 
 });
 
-emitter.on('loggerEvent', (arg) => {
-    console.log('logging event listener called', arg);
-});
 
-//Raise a logger event
-emitter.emit('loggerEvent', {
-    data: 'message'
-});
-
-//Raise an event
-emitter.emit('messageLogged', {
-    id: 1,
-    url: 'http://'
-});
+logger.log('message');
