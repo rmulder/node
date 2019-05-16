f77374e1fc3691d0e3e908e03ea6866af52cc3d1
sivachallapalli <siva.apps@icloud.com>
2019-05-16 08:21:18 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function sayHello(name) {
+    console.log('Hello' + name);
+}
+
+sayHello('Siva');
+// console.log(window);
+
+global.console.log("test");
+var message = "test";
+
+console.log(message);
