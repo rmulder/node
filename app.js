e271b3f65432187f92fe4605dccd960ae5b5f150
sivachallapalli <siva.apps@icloud.com>
2019-05-16 08:26:22 -0500

    module

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-function sayHello(name) {
-    console.log('Hello' + name);
-}
-
-sayHello('Siva');
-// console.log(window);
-
-global.console.log("test");
-var message = "test";
-
-console.log(message);
+console.log(module);
