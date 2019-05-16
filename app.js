51ec82cef4f4a708716227c9207f24080bb42c07
sivachallapalli <siva.apps@icloud.com>
2019-05-16 14:11:44 -0500

    event arguments

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ const emitter = new EventEmitter();
 
 
 //Register a listener
-emitter.on('messageLogged', function () {
-    console.log('Listener called');
+emitter.on('messageLogged', (arg) => { //e, eventArg
+    console.log('Listener called', arg);
 
 });
 
+emitter.on('loggerEvent', (arg) => {
+    console.log('logging event listener called', arg);
+});
+
+//Raise a logger event
+emitter.emit('loggerEvent', {
+    data: 'message'
+});
+
 //Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {
+    id: 1,
+    url: 'http://'
+});
