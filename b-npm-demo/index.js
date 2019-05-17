2e0517f9dc17ea013c74b9e8c9cdff17da9245b6
sivachallapalli <siva.apps@icloud.com>
2019-05-17 11:42:33 -0500

    publish node package

---
+++ b-npm-demo/index.js
@@ -0,0 +1,7 @@
+const _ = require('underscore');
+const lion = require('c-lion-lib');
+
+var result = _.contains([1, 2], 2);
+
+var result2 = lion.add(1, 2);
+console.log(result2);
--- npm-demo/index.js
+++
@@ -1,4 +0,0 @@
-const _ = require('underscore');
-
-var result = _.contains([1, 2], 2);
-console.log(result);
