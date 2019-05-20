103f1e9a79dd71722a4df1b055f980ed9aed14f9
sivachallapalli <siva.apps@icloud.com>
2019-05-20 18:04:42 -0500

    handling delete requests

--- d-express-demo/index.js
+++ d-express-demo/index.js
@@ -59,6 +59,22 @@ app.put('/api/courses/:id', (req, res) => {
   res.send(course);
 });
 
+
+
+app.delete('/api/courses/:id', (req, res) => {
+  //404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+
+});
+
+
+
 function validateCourse(course) {
   const schema = {
     name: Joi.string().min(3).required()
