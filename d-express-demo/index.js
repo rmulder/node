f511c58c1127d1cbe84f8f362ff921932d41d3c1
sivachallapalli <siva.apps@icloud.com>
2019-05-20 17:07:06 -0500

    input validation

--- d-express-demo/index.js
+++ d-express-demo/index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [{
     id: 1,
@@ -22,6 +25,40 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
   res.send(courses);
 });
+
+app.post('/api/courses', (req, res) => {
+
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+
+
+  // if (!req.body.name || req.body.name.length < 3) {
+  //   res.status(400).send('Name is required and should be minimum 3 characters.');
+  //   return;
+  // }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+
+
+
+
+
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
