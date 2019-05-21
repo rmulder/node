80e0b668c1f9f6e899482509b45bab8f78cbbaf5
sivachallapalli <siva.apps@icloud.com>
2019-05-21 18:20:08 -0500

    structuring express application

--- d-express-demo/index.js
+++ d-express-demo/index.js
@@ -2,9 +2,12 @@ const config = require('config');
 const debug = require('debug')('app:startup');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const Joi = require('joi');
-const logger = require('./logger');
-const auth = require('./authentication');
+// const Joi = require('joi');
+const logger = require('./middleware/logger');
+const auth = require('./middleware/authentication');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
+
 const express = require('express');
 const app = express();
 
@@ -18,13 +21,6 @@ app.use(express.urlencoded({
 app.use(helmet());
 app.use(express.static('public'));
 
-
-//Configuration
-console.log(`Application Name: ${config.get('name')}`);
-console.log(`Mail Server: ${config.get('mail.host')}`);
-console.log(`Mail Password: ${config.get('mail.password')}`);
-
-
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
   debug('Morgan enabled...');
@@ -32,89 +28,14 @@ if (app.get('env') === 'development') {
 app.use(logger);
 app.use(auth);
 
-
-const courses = [{
-    id: 1,
-    name: 'course1'
-  },
-  {
-    id: 2,
-    name: 'course2'
-  },
-  {
-    id: 3,
-    name: 'course3'
-  }
-];
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'My Express App',
-    message: 'Hello'
-  });
-});
-
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-  //400
-  const {
-    error
-  } = validateCourse(req.body);
-
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-  //404
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found');
+app.use('/api/courses', courses);
+app.use('/', home);
 
 
-  //400
-  const {
-    error
-  } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-
-  course.name = req.body.name;
-  res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-  //404
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found');
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
-
-});
-
-function validateCourse(course) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-  return Joi.validate(course, schema);
-}
-
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found');
-  res.send(course);
-});
+//Configuration
+console.log(`Application Name: ${config.get('name')}`);
+console.log(`Mail Server: ${config.get('mail.host')}`);
+console.log(`Mail Password: ${config.get('mail.password')}`);
 
 //PORT
 const port = process.env.PORT || 3000;
