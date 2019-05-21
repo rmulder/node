6a4f2e195eb2742fd60b729c15eb34675f7e16e4
sivachallapalli <siva.apps@icloud.com>
2019-05-20 23:23:23 -0500

    vidly project final

--- e-project-vidly/index.js
+++ e-project-vidly/index.js
@@ -2,6 +2,8 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const genres = [{
     id: 1,
     name: "Horror"
@@ -30,17 +32,56 @@ app.get('/api/genres/:id', (req, res) => {
   res.send(genre);
 });
 
-app.update('/api/genres', (req, res) => {
+app.post('/api/genres', (req, res) => {
+  //400
+  const {
+    error
+  } = validateGenre(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+  //404
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  //400
+  const {
+    error
+  } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //update
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+  //404
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  //delete
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res.send(genre);
+});
+
+function validateGenre(genre) {
   const schema = {
     name: Joi.string().min(3).required()
   };
+  return Joi.validate(genre, schema);
+}
 
-  const result = Joi.validate(schema);
-  if (result.error) return res.status(400).send(result.error.details[0].message);
-
-  const id = genres.length + 1;
-
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
